8e9d890028e4edc427e2906330f59bbea3b8e9b4
Dalia Caspriac <Dalia Caspriac@DESKTOP-AAIIOBM>
2017-05-02 21:55:32 +0300

    added Model.Anonim class

---
+++ ConferenceManagement/ConferenceManagement/Model/Anonim.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceManagement.Model
+{
+    public class Anonim
+    {
+        private int id;
+
+        public Anonim(int id)
+        {
+            this.Id = id;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+    }
+}
