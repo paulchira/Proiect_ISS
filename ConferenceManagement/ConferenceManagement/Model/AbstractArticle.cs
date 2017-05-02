52aae15efab36a818c482238b594a5edea4ca5c0
Dalia Caspriac <Dalia Caspriac@DESKTOP-AAIIOBM>
2017-05-02 23:36:56 +0300

    added Model.AbstractArticle class

---
+++ ConferenceManagement/ConferenceManagement/Model/AbstractArticle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceManagement.Model
+{
+    public class AbstractArticle
+    {
+        private int id;
+        private string title;
+        private List<string> keywords;
+        private string topics;
+        private string author;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public List<string> Keywords
+        {
+            get
+            {
+                return keywords;
+            }
+
+            set
+            {
+                keywords = value;
+            }
+        }
+
+        public string Topics
+        {
+            get
+            {
+                return topics;
+            }
+
+            set
+            {
+                topics = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                author = value;
+            }
+        }
+    }
+}
