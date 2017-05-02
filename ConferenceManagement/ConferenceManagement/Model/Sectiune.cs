a450c22f33d5bcb20df653faba6bd093d63b735e
paulchira <paulchira_2007@yahoo.com>
2017-05-02 22:05:38 +0300

    added Model.Sectiune class

---
+++ ConferenceManagement/ConferenceManagement/Model/Sectiune.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceManagement.Model
+{
+    public class Sectiune
+    {
+        private int id;
+        private String tema;
+        private String sala;
+        private int nrParticipanti;
+        public Sectiune(int id, String tema, String sala, int nrParticipanti)
+        {
+            this.id = id;
+            this.tema = tema;
+            this.sala = sala;
+            this.nrParticipanti = nrParticipanti;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Tema
+        {
+            get
+            {
+                return tema;
+            }
+
+            set
+            {
+                tema = value;
+            }
+        }
+
+        public string Sala
+        {
+            get
+            {
+                return sala;
+            }
+
+            set
+            {
+                sala = value;
+            }
+        }
+
+        public int NrParticipanti
+        {
+            get
+            {
+                return nrParticipanti;
+            }
+
+            set
+            {
+                nrParticipanti = value;
+            }
+        }
+    }
+}
