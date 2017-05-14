b6fbf3c8a32d1cbf648908c2f8bc21378081b491
lauracernau <lauracernau@gmail.com>
2017-05-14 22:49:21 +0300

    Database connection
    Creating the connection to the database using a configuration file
    (App.config)

---
+++ ConferenceManagement/ConferenceManagement/Persistance/DatabaseConnection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ConferenceManagement.Persistance
+{
+   /// <summary>
+   /// Singleton class for creating a databse connection.
+   /// </summary>
+    public static class DatabaseConnection
+    {
+       
+        private static IDbConnection instance = null;
+        /// <summary>
+        /// Gets the connection to the database.
+        /// </summary>
+        /// <returns>The connection to the data source</returns>
+        public static IDbConnection getConnection()
+        {
+            if (instance == null || instance.State == System.Data.ConnectionState.Closed)
+            {
+                instance = getNewConnection();
+                instance.Open();
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Creats a new connection to the data source.
+        /// </summary>
+        /// <returns>The connection to the data source</returns>
+        private static IDbConnection getNewConnection()
+        {
+            string con = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+            return new SqlConnection(con);
+        }
+    }
+}
--- ConferenceManagement/ConferenceManagement/Program.cs
+++ ConferenceManagement/ConferenceManagement/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ConferenceManagement.Persistance;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,6 +19,8 @@ namespace ConferenceManagement
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+           
         }
     }
 }
