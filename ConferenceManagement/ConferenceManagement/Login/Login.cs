6b968d5e1acaf07282fc6ee252e0005cc8753071
Alessandro <alexalghisi@yahoo.com>
2017-05-15 19:32:21 +0300

    Homeworks => added Login class :)

---
+++ ConferenceManagement/ConferenceManagement/Login/Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceManagement.Login
+{
+    class Login
+    {
+        SqlConnection cs = new SqlConnection("Data Source=localhost;Initial Catalog=Conference_Management;Integrated Security=True");
+
+        public Login() { }
+
+        // Return true if user & pass are found in DB or false otherwise.
+        // Params :
+        //   @user = username
+        //   @pass = password
+        public bool Auth(string user, string pass)
+        {
+            SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) from Users where username=@user and password=@pass", cs);
+            cs.Open();
+            sqlCommand.Parameters.Add("@user", SqlDbType.VarChar).Value = user;
+            sqlCommand.Parameters.Add("@pass", SqlDbType.VarChar).Value = pass;
+            
+            int userCount = (int)sqlCommand.ExecuteScalar();
+            Console.WriteLine(userCount);
+            if (userCount > 0)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ ConferenceManagement/ConferenceManagement/Model/Listener.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceManagement.Model
+{
+    class Listener
+    {
+        private int id;
+        private String nume;
+        private String prenume;
+        private String domeniu;
+        private int nrArticole;
+        private int nrPremii;
+
+        public Listener(int id, String nume, String prenume, String domeniu, int nrArticole, int nrPremii)
+        {
+            this.id = id;
+            this.nume = nume;
+            this.prenume = prenume;
+            this.domeniu = domeniu;
+            this.NrArticole = nrArticole;
+            this.NrPremii = nrPremii;
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Nume
+        {
+            get
+            {
+                return nume;
+            }
+
+            set
+            {
+                nume = value;
+            }
+        }
+
+        public string Domeniu
+        {
+            get
+            {
+                return domeniu;
+            }
+
+            set
+            {
+                domeniu = value;
+            }
+        }
+
+        public int NrArticole
+        {
+            get
+            {
+                return nrArticole;
+            }
+
+            set
+            {
+                nrArticole = value;
+            }
+        }
+
+        public int NrPremii
+        {
+            get
+            {
+                return nrPremii;
+            }
+
+            set
+            {
+                nrPremii = value;
+            }
+        }
+    }
+}
