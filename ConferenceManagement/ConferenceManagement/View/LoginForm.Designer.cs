7cb065f08200217952a681ce173859b271c80117
lauracernau <lauracernau@gmail.com>
2017-05-22 00:43:17 +0300

    Login form gui.

---
+++ ConferenceManagement/ConferenceManagement/View/LoginForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ConferenceManagement.View
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username_textBox = new System.Windows.Forms.TextBox();
+            this.password_textBox = new System.Windows.Forms.TextBox();
+            this.login_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " Password: ";
+            // 
+            // username_textBox
+            // 
+            this.username_textBox.Location = new System.Drawing.Point(168, 91);
+            this.username_textBox.Name = "username_textBox";
+            this.username_textBox.Size = new System.Drawing.Size(100, 20);
+            this.username_textBox.TabIndex = 2;
+            // 
+            // password_textBox
+            // 
+            this.password_textBox.Location = new System.Drawing.Point(168, 142);
+            this.password_textBox.Name = "password_textBox";
+            this.password_textBox.Size = new System.Drawing.Size(100, 20);
+            this.password_textBox.TabIndex = 3;
+            // 
+            // login_button
+            // 
+            this.login_button.BackColor = System.Drawing.Color.SkyBlue;
+            this.login_button.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_button.Location = new System.Drawing.Point(222, 216);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(96, 35);
+            this.login_button.TabIndex = 4;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(447, 336);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.password_textBox);
+            this.Controls.Add(this.username_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username_textBox;
+        private System.Windows.Forms.TextBox password_textBox;
+        private System.Windows.Forms.Button login_button;
+    }
+}
