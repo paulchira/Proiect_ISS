d67d963d5e772eb41e5b199a961e6b3db64d58e4
Chiorean Adrian <Chiorean Adrian@DESKTOP-NHOK1EU>
2017-05-21 20:44:49 +0300

    repository pcmember + update pcmember class

---
+++ ConferenceManagement/ConferenceManagement/Persistance/IRepositoryPCMember.cs
@@ -0,0 +1,14 @@
+﻿using ConferenceManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceManagement.Persistance
+{
+    public interface IRepositoryPCMember : ICrudRepository<PCMember>
+    {
+        
+    }
+}
