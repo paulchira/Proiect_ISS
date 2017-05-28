57c234b5a20920ddbfc6a6e63e9e8881bd5ccf9b
paulchira <paulchira_2007@yahoo.com>
2017-05-28 17:32:29 +0300

    updated clients Controllers

--- ConferenceManagement/ConferenceManagement/View/ParticipantView/ControllerParticipant.cs
+++ ConferenceManagement/ConferenceManagement/View/ParticipantView/ControllerParticipant.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConferenceManagement.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ConferenceManagement.View.ParticipantView
 {
-    public class ControllerParticipant
+    public class ControllerParticipant:IServiceParticipant
     {
     }
 }
--- ConferenceManagement/ConferenceManagement/View/ReviewerView/ControllerReviewer.cs
+++ ConferenceManagement/ConferenceManagement/View/ReviewerView/ControllerReviewer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConferenceManagement.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ConferenceManagement.View.ReviewerView
 {
-    public class ControllerReviewer
+    public class ControllerReviewer:IServiceReviewer
     {
     }
 }
