900f7cdeaa44736c1996b973da77982cca745c3f
paulchira <paulchira_2007@yahoo.com>
2017-05-28 18:19:41 +0300

    create console project for server

---
+++ ConferenceManagement/ServerConferenceManagement/StartServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConferenceManagement
+{
+    class StartServer
+    {
+        static void Main(string[] args)
+        {
+            // net remoting
+            BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
+            serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+            BinaryClientFormatterSinkProvider clientProv = new BinaryClientFormatterSinkProvider();
+            IDictionary props = new Hashtable();
+
+            props["port"] = 55555;
+            TcpChannel channel = new TcpChannel(props, clientProv, serverProv);
+            ChannelServices.RegisterChannel(channel, false);
+
+            var server = new ServiceServerImpl();
+            RemotingServices.Marshal(server, "ConferenceManagement Server");
+            Console.WriteLine("Server started ...");
+            Console.WriteLine("Press <enter> to exit...");
+            Console.ReadLine();
+        }
+    }
+}
