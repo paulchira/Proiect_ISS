ad766442815cbebebfdadcd0a2b0794108f831b5
Chiorean Adrian <Chiorean Adrian@DESKTOP-NHOK1EU>
2017-06-01 12:42:14 +0300

    review article form updated

--- ConferenceManagement/ConferenceManagement/View/ReviewerView/ReviewArticleForm.cs
+++ ConferenceManagement/ConferenceManagement/View/ReviewerView/ReviewArticleForm.cs
@@ -15,10 +15,13 @@ namespace ConferenceManagement.View.ReviewerView
     public partial class ReviewArticleForm : Form
     {
         ClientController ctrl;
-        public ReviewArticleForm(ClientController ctr)
+        Article article;
+        public ReviewArticleForm(ClientController ctr, Article article)
         {
             InitializeComponent();
             ctrl = ctr;
+            this.article = article;
+            textBox_ArticleTitle.Text = article.ArticleTitle;
         }
     }
 }
