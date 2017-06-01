65e3221f4f43bde404dece769d548f210cbac868
Alessandro <alexalghisi@yahoo.com>
2017-06-01 13:11:17 +0300

    Testing add review

--- ConferenceManagement/ConferenceManagement/View/ReviewerView/ReviewArticleForm.Designer.cs
+++ ConferenceManagement/ConferenceManagement/View/ReviewerView/ReviewArticleForm.Designer.cs
@@ -35,30 +35,32 @@
             this.SubmitReview_button = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.checkBox6 = new System.Windows.Forms.CheckBox();
             this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox_ArticleTitle
             // 
-            this.textBox_ArticleTitle.Location = new System.Drawing.Point(127, 31);
+            this.textBox_ArticleTitle.Location = new System.Drawing.Point(169, 38);
+            this.textBox_ArticleTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_ArticleTitle.Name = "textBox_ArticleTitle";
-            this.textBox_ArticleTitle.Size = new System.Drawing.Size(211, 20);
+            this.textBox_ArticleTitle.Size = new System.Drawing.Size(280, 22);
             this.textBox_ArticleTitle.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Location = new System.Drawing.Point(16, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 23);
+            this.label1.Size = new System.Drawing.Size(118, 30);
             this.label1.TabIndex = 1;
             this.label1.Text = "Article title:";
             // 
@@ -66,9 +68,10 @@
             // 
             this.downloadArticle_button.BackColor = System.Drawing.Color.SkyBlue;
             this.downloadArticle_button.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.downloadArticle_button.Location = new System.Drawing.Point(380, 24);
+            this.downloadArticle_button.Location = new System.Drawing.Point(507, 30);
+            this.downloadArticle_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.downloadArticle_button.Name = "downloadArticle_button";
-            this.downloadArticle_button.Size = new System.Drawing.Size(120, 32);
+            this.downloadArticle_button.Size = new System.Drawing.Size(160, 39);
             this.downloadArticle_button.TabIndex = 2;
             this.downloadArticle_button.Text = "Download article";
             this.downloadArticle_button.UseVisualStyleBackColor = false;
@@ -76,9 +79,10 @@
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.Color.PowderBlue;
-            this.richTextBox1.Location = new System.Drawing.Point(103, 99);
+            this.richTextBox1.Location = new System.Drawing.Point(137, 122);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(382, 91);
+            this.richTextBox1.Size = new System.Drawing.Size(508, 111);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
@@ -86,20 +90,23 @@
             // 
             this.SubmitReview_button.BackColor = System.Drawing.Color.SkyBlue;
             this.SubmitReview_button.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SubmitReview_button.Location = new System.Drawing.Point(380, 376);
+            this.SubmitReview_button.Location = new System.Drawing.Point(507, 463);
+            this.SubmitReview_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SubmitReview_button.Name = "SubmitReview_button";
-            this.SubmitReview_button.Size = new System.Drawing.Size(170, 38);
+            this.SubmitReview_button.Size = new System.Drawing.Size(227, 47);
             this.SubmitReview_button.TabIndex = 4;
             this.SubmitReview_button.Text = "Submit my review";
             this.SubmitReview_button.UseVisualStyleBackColor = false;
+            this.SubmitReview_button.Click += new System.EventHandler(this.SubmitReview_button_Click_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 96);
+            this.label2.Location = new System.Drawing.Point(17, 118);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 23);
+            this.label2.Size = new System.Drawing.Size(109, 30);
             this.label2.TabIndex = 5;
             this.label2.Text = "Comments:";
             // 
@@ -112,89 +119,98 @@
             this.groupBox1.Controls.Add(this.checkBox3);
             this.groupBox1.Controls.Add(this.checkBox2);
             this.groupBox1.Controls.Add(this.checkBox1);
-            this.groupBox1.Location = new System.Drawing.Point(67, 217);
+            this.groupBox1.Location = new System.Drawing.Point(89, 267);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(452, 127);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(603, 156);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Qualifier";
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(17, 29);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(93, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Strong accept";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(17, 63);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(60, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Accept";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(17, 95);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(91, 17);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "Weak accept";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox4
-            // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(170, 29);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(100, 17);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "Bordeline paper";
-            this.checkBox4.UseVisualStyleBackColor = true;
-            // 
-            // checkBox5
-            // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(170, 63);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(84, 17);
-            this.checkBox5.TabIndex = 4;
-            this.checkBox5.Text = "Weak reject";
-            this.checkBox5.UseVisualStyleBackColor = true;
-            // 
-            // checkBox6
-            // 
-            this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(287, 29);
-            this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(57, 17);
-            this.checkBox6.TabIndex = 5;
-            this.checkBox6.Text = "Reject";
-            this.checkBox6.UseVisualStyleBackColor = true;
-            // 
             // checkBox7
             // 
             this.checkBox7.AutoSize = true;
-            this.checkBox7.Location = new System.Drawing.Point(287, 63);
+            this.checkBox7.Location = new System.Drawing.Point(383, 78);
+            this.checkBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox7.Name = "checkBox7";
-            this.checkBox7.Size = new System.Drawing.Size(86, 17);
+            this.checkBox7.Size = new System.Drawing.Size(111, 21);
             this.checkBox7.TabIndex = 6;
             this.checkBox7.Text = "Strong reject";
             this.checkBox7.UseVisualStyleBackColor = true;
             // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(383, 36);
+            this.checkBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(70, 21);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Reject";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(227, 78);
+            this.checkBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(105, 21);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Weak reject";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(227, 36);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(131, 21);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Bordeline paper";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(23, 117);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(112, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Weak accept";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(23, 78);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(73, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Accept";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 36);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(118, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Strong accept";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // ReviewArticleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(562, 460);
+            this.ClientSize = new System.Drawing.Size(749, 566);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.SubmitReview_button);
@@ -202,6 +218,7 @@
             this.Controls.Add(this.downloadArticle_button);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox_ArticleTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ReviewArticleForm";
             this.Text = "ReviewArticleForm";
             this.groupBox1.ResumeLayout(false);
